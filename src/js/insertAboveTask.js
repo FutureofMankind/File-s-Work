0589bd92a79cb8689f16f243bad22f15a3e5c5f1
Stanislav Petrov <marcusflaviusaquilaiii@gmail.com>
2024-11-21 03:07:33 +0500

    Done!

---
+++ src/js/insertAboveTask.js
@@ -0,0 +1,18 @@
+export default function insertAboveTask(zone, mouseY) {
+  const notActualElements = zone.querySelectorAll(".card:not(.is-dragging)");
+  let closestCard = null;
+  let closestOffset = Number.NEGATIVE_INFINITY;
+
+  notActualElements.forEach((elem) => {
+    const { top } = elem.getBoundingClientRect();
+
+    const offset = mouseY - top;
+
+    if (offset < 0 && offset > closestOffset) {
+      closestOffset = offset;
+      closestCard = elem;
+    }
+  });
+
+  return closestCard;
+}
